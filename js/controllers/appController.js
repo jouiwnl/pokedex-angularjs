ea9e2568e2745c27f1fbc84499ac4cf8312c5c85
unknown <jou.098olo@gmail.com>
2021-07-08 21:44:54 -0300

    adição de botões para stats do pokemon procurado

--- js/controllers/appController.js
+++ js/controllers/appController.js
@@ -1,6 +1,12 @@
 angular.module("appPokedex").controller("appController", function($scope, pokemonService) {
     $scope.pokemons = [];
     $scope.pokemon = {};
+    $scope.showPokemonInfo = true;
+    $scope.showPokemonHP = false;
+    $scope.showPokemonAttack = false;
+    $scope.showPokemonStrenght = false;
+    $scope.showPokemonDefense = false;
+    $scope.showPokemonSpeed = false;
 
     $scope.loadSingle = (nome) => {
         pokemonService.findSingle(nome).then((response) => {
@@ -19,6 +25,58 @@ angular.module("appPokedex").controller("appController", function($scope, pokemo
         $scope.loadSingle(nome);
 
         $scope.showPokemonPhoto = true;
+        $scope.showPokemonAttack = false;
+        $scope.showPokemonHP = false;
+        $scope.showPokemonStrenght = false;
+        $scope.showPokemonDefense = false;
+        $scope.showPokemonSpeed = false;
+        $scope.showPokemonInfo = true;
     }
+
+    $scope.attackPokemon = () => {
+        $scope.showPokemonInfo = false;
+        $scope.showPokemonHP = false;
+        $scope.showPokemonStrenght = false;
+        $scope.showPokemonDefense = false;
+        $scope.showPokemonSpeed = false;
+        $scope.showPokemonAttack = true;
+    }
+
+    $scope.hpPokemon = () => {
+        $scope.showPokemonInfo = false;
+        $scope.showPokemonAttack = false;
+        $scope.showPokemonStrenght = false;
+        $scope.showPokemonDefense = false;
+        $scope.showPokemonSpeed = false;
+        $scope.showPokemonHP = true; 
+    }
+
+    $scope.strenghtPokemon = () => {
+        $scope.showPokemonInfo = false;
+        $scope.showPokemonAttack = false;
+        $scope.showPokemonHP = false;
+        $scope.showPokemonDefense = false;
+        $scope.showPokemonSpeed = false;
+        $scope.showPokemonStrenght = true;
+    }
+
+    $scope.defensePokemon = () => {
+        $scope.showPokemonInfo = false;
+        $scope.showPokemonAttack = false;
+        $scope.showPokemonHP = false;
+        $scope.showPokemonStrenght = false;
+        $scope.showPokemonSpeed = false;
+        $scope.showPokemonDefense = true;
+    }
+
+    $scope.speedPokemon = () => {
+        $scope.showPokemonInfo = false;
+        $scope.showPokemonAttack = false;
+        $scope.showPokemonHP = false;
+        $scope.showPokemonStrenght = false;
+        $scope.showPokemonDefense = false;
+        $scope.showPokemonSpeed = true;
+    }
+
    
 });
